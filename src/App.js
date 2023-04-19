b2f89e4ffcc595bbfc72539c09c3f891d173d364
BhaumikMuliya <bhaumikmuliya1304+github@gmail.com>
2023-04-20 01:53:56 +0530

    homepage UI for desktop

--- src/App.js
+++ src/App.js
@@ -1,5 +1,19 @@
 function App() {
-  return <p>Hello world!</p>;
+  return (
+    <main className="grid w-screen h-screen gap-10 text-center font-cursive place-content-center bg-neutral-300">
+      <p className="text-lg font-bold">
+        Click on any of button below and go to relative component
+      </p>
+      <div className="flex justify-center gap-5">
+        <button className="px-4 py-2 border-2 border-black rounded-lg shadow-button hover:translate-x-1 hover:translate-y-1 hover:shadow-none">
+          Telephone
+        </button>
+        <button className="px-4 py-2 border-2 border-black rounded-lg shadow-button hover:translate-x-1 hover:translate-y-1 hover:shadow-none">
+          Wire (Coming soon)
+        </button>
+      </div>
+    </main>
+  );
 }
 
 export default App;
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,14 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        cursive: "cursive",
+      },
+      boxShadow: {
+        button: "4px 4px black",
+      },
+    },
   },
   plugins: [],
 };
